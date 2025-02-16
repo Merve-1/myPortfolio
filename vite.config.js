83e8f16a569c94154486b124f6b940247bd85683
Marwa Alazab <104984704+Merve-1@users.noreply.github.com>
2025-02-16 20:08:47 +0200

    Add missing assets

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
+  base: '/myPortfolio/', // Correct placement of base
   plugins: [react()],
   build: {
-    base: '/myPortfolio/', // Replace with your repository name
     chunkSizeWarningLimit: 1000, // Set a higher limit (default is 500)
   },
 });
