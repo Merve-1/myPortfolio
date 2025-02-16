7e84eee82a3f7f717599234de935fd802204065c
Marwa Alazab <104984704+Merve-1@users.noreply.github.com>
2025-02-16 19:30:21 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   build: {
+    base: '/myPortfolio/', // Replace with your repository name
     chunkSizeWarningLimit: 1000, // Set a higher limit (default is 500)
   },
 });
